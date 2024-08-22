1721c01b6123fdf905fce827826404b97c3c04c8
Bruno Martins <bgcngm@gmail.com>
2024-08-22 23:46:53 +0100

    drivers: touchscreen: Revert latest OOS 14.0.0.810(EX01) update
    For some reason, even with kernel_grip_support disabled, there is
    something causing touchpanel sensitivity issues.
    
    Change-Id: I835d5cf609e258243445fecab40701c85cf40632

--- drivers/input/touchscreen/synaptics_hbp/tcm/synaptics_touchcom_func_base.c
+++ drivers/input/touchscreen/synaptics_hbp/tcm/synaptics_touchcom_func_base.c
@@ -1354,7 +1354,7 @@ int syna_tcm_set_dynamic_config(struct tcm_dev *tcm_dev,
 		return _EINVAL;
 	}
 
-	LOGI("Set %d to dynamic field 0x%x\n", value, id);
+	LOGD("Set %d to dynamic field 0x%x\n", value, id);
 
 	out[0] = (unsigned char)id;
 	out[1] = (unsigned char)value;
